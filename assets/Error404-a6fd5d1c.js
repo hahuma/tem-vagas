0972ebf23024442ccfeb9c459a5d284d83033452
Hahuma <firminopetterson@gmail.com>
2023-01-30 19:48:00 -0300

    initial commit

---
+++ assets/Error404-a6fd5d1c.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./runtime-core.esm-bundler-5fc0a256.js";const r={class:"home"},a={__name:"Error404",setup(t){return(c,s)=>(e(),o("div",r," ERROR 404 "))}};export{a as default};
